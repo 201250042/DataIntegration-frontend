7f6bb31f78a9bf2b32e9607b715bc1f6ee7dab5a
cesium <cesium-137@foxmail.com>
2024-01-09 21:56:15 +0800

    美化“课程选课学生院系分布”界面，功能点3.2填空改下拉框

--- src/view/charts/InstituteCount.vue
+++ src/view/charts/InstituteCount.vue
@@ -14,11 +14,53 @@
         >
         </el-option>
       </el-select>
-      <el-input
+<!--      <el-input-->
+<!--        v-model="course"-->
+<!--        placeholder="课程编号"-->
+<!--        style="width:120px;"-->
+<!--      ></el-input>-->
+      <el-select
         v-model="course"
         placeholder="课程编号"
-        style="width:120px;"
-      ></el-input>
+        style="margin-right: 10px;width:113px"
+      >
+        <div v-if="selectedInstitute === 'A'">
+          <el-option
+            v-for="item in coursesA"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </div>
+        <div v-else-if="selectedInstitute === 'B'">
+          <el-option
+            v-for="item in coursesB"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </div>
+        <div v-else-if="selectedInstitute === 'C'">
+          <el-option
+            v-for="item in coursesC"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </div>
+        <div v-else-if="selectedInstitute === 'ABC'">
+          <el-option
+            v-for="item in courses"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </div>
+      </el-select>
       <el-button
         type="primary"
         @click="query"
@@ -36,8 +78,14 @@
 
 <script>
 import { getStudentDistributionFromA, getStudentDistributionFromB, getStudentDistributionFromC, getStudentDistributionFromAll } from '@/network/courses/index.js';
+import it from "element-ui/src/locale/lang/it";
 
 export default {
+  computed: {
+    it() {
+      return it
+    }
+  },
   data() {
     return {
       options: [
@@ -57,6 +105,262 @@ export default {
           value: 'ABC',
           label: '所有学院'
         },
+      ],
+
+      coursesA: [
+        // A学院
+        {
+          value: '1001',
+          label: '1001'
+        },
+        {
+          value: '1002',
+          label: '1002'
+        },
+        {
+          value: '1003',
+          label: '1003'
+        },
+        {
+          value: '1004',
+          label: '1004'
+        },
+        {
+          value: '1005',
+          label: '1005'
+        },
+        {
+          value: '1006',
+          label: '1006'
+        },
+        {
+          value: '1007',
+          label: '1007'
+        },
+        {
+          value: '1008',
+          label: '1008'
+        },
+        {
+          value: '1009',
+          label: '1009'
+        },
+        {
+          value: '1010',
+          label: '1010'
+        }
+      ],
+      coursesB: [
+        // B学院
+        {
+          value: '2001',
+          label: '2001'
+        },
+        {
+          value: '2002',
+          label: '2002'
+        },
+        {
+          value: '2003',
+          label: '2003'
+        },
+        {
+          value: '2004',
+          label: '2004'
+        },
+        {
+          value: '2005',
+          label: '2005'
+        },
+        {
+          value: '2006',
+          label: '2006'
+        },
+        {
+          value: '2007',
+          label: '2007'
+        },
+        {
+          value: '2008',
+          label: '2008'
+        },
+        {
+          value: '2009',
+          label: '2009'
+        },
+        {
+          value: '2010',
+          label: '2010'
+        }
+      ],
+      coursesC: [
+        // C学院
+        {
+          value: '3001',
+          label: '3001'
+        },
+        {
+          value: '3002',
+          label: '3002'
+        },
+        {
+          value: '3003',
+          label: '3003'
+        },
+        {
+          value: '3004',
+          label: '3004'
+        },
+        {
+          value: '3005',
+          label: '3005'
+        },
+        {
+          value: '3006',
+          label: '3006'
+        },
+        {
+          value: '3007',
+          label: '3007'
+        },
+        {
+          value: '3008',
+          label: '3008'
+        },
+        {
+          value: '3009',
+          label: '3009'
+        },
+        {
+          value: '3010',
+          label: '3010'
+        }
+      ],
+
+      courses: [
+        // A学院
+        {
+          value: '1001',
+          label: '1001'
+        },
+        {
+          value: '1002',
+          label: '1002'
+        },
+        {
+          value: '1003',
+          label: '1003'
+        },
+        {
+          value: '1004',
+          label: '1004'
+        },
+        {
+          value: '1005',
+          label: '1005'
+        },
+        {
+          value: '1006',
+          label: '1006'
+        },
+        {
+          value: '1007',
+          label: '1007'
+        },
+        {
+          value: '1008',
+          label: '1008'
+        },
+        {
+          value: '1009',
+          label: '1009'
+        },
+        {
+          value: '1010',
+          label: '1010'
+        },
+        // B学院
+        {
+          value: '2001',
+          label: '2001'
+        },
+        {
+          value: '2002',
+          label: '2002'
+        },
+        {
+          value: '2003',
+          label: '2003'
+        },
+        {
+          value: '2004',
+          label: '2004'
+        },
+        {
+          value: '2005',
+          label: '2005'
+        },
+        {
+          value: '2006',
+          label: '2006'
+        },
+        {
+          value: '2007',
+          label: '2007'
+        },
+        {
+          value: '2008',
+          label: '2008'
+        },
+        {
+          value: '2009',
+          label: '2009'
+        },
+        {
+          value: '2010',
+          label: '2010'
+        },
+        // C学院
+        {
+          value: '3001',
+          label: '3001'
+        },
+        {
+          value: '3002',
+          label: '3002'
+        },
+        {
+          value: '3003',
+          label: '3003'
+        },
+        {
+          value: '3004',
+          label: '3004'
+        },
+        {
+          value: '3005',
+          label: '3005'
+        },
+        {
+          value: '3006',
+          label: '3006'
+        },
+        {
+          value: '3007',
+          label: '3007'
+        },
+        {
+          value: '3008',
+          label: '3008'
+        },
+        {
+          value: '3009',
+          label: '3009'
+        },
+        {
+          value: '3010',
+          label: '3010'
+        }
       ],
       selectedInstitute: '',
       course: '',
